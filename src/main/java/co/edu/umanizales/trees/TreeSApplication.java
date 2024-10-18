006fd1b4b00ff160697ef4adc2c5e3d08f6c4dd2
sofiaocampo13 <socampo103562@umanizales.edu.co>
2024-10-18 09:09:33 -0500

    Initial commit

---
+++ src/main/java/co/edu/umanizales/trees/TreeSApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.trees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/umanizales/trees/TreeSApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.trees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
